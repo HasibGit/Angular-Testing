4430560d96d0763aa40d3802e0acd8ce4da13490
hasib ullah <u1176456@internal.imsglobal.com>
2024-06-15 13:24:56 +0600

    covered how to test promise and settimeout together

--- src/app/courses/home/async-test.spec.ts
+++ src/app/courses/home/async-test.spec.ts
@@ -46,3 +46,29 @@ describe("Test Async Operations", () => {
     expect(test).toBeTruthy();
   }));
 });
+
+it("promise + setTimeout()", fakeAsync(() => {
+  let counter = 0;
+
+  Promise.resolve().then(() => {
+    counter += 10;
+
+    setTimeout(() => {
+      counter += 1;
+    }, 1000);
+  });
+
+  expect(counter).toBe(0);
+
+  flushMicrotasks();
+
+  expect(counter).toBe(10);
+
+  tick(500);
+
+  expect(counter).toBe(10);
+
+  tick(500);
+
+  expect(counter).toBe(11);
+}));
