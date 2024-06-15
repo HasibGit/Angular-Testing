3f00c9674d5f422041d25a09aaa3b2c66e8b0998
hasib ullah <u1176456@internal.imsglobal.com>
2024-06-15 13:45:57 +0600

    testing observables covered

--- src/app/courses/home/async-test.spec.ts
+++ src/app/courses/home/async-test.spec.ts
@@ -1,4 +1,6 @@
 import { fakeAsync, flush, flushMicrotasks, tick } from "@angular/core/testing";
+import { of } from "rxjs";
+import { delay } from "rxjs/operators";
 
 describe("Test Async Operations", () => {
   it("Should make it true", fakeAsync(() => {
@@ -45,30 +47,43 @@ describe("Test Async Operations", () => {
 
     expect(test).toBeTruthy();
   }));
+
+  it("promise + setTimeout()", fakeAsync(() => {
+    let counter = 0;
+
+    Promise.resolve().then(() => {
+      counter += 10;
+
+      setTimeout(() => {
+        counter += 1;
+      }, 1000);
+    });
+
+    expect(counter).toBe(0);
+
+    flushMicrotasks();
+
+    expect(counter).toBe(10);
+
+    tick(500);
+
+    expect(counter).toBe(10);
+
+    tick(500);
+
+    expect(counter).toBe(11);
+  }));
+
+  it("Testing observables", fakeAsync(() => {
+    let test = false;
+
+    const test$ = of(test).pipe(delay(1000));
+
+    test$.subscribe(() => {
+      test = true;
+    });
+
+    tick(1000);
+    expect(test).toBeTruthy();
+  }));
 });
-
-it("promise + setTimeout()", fakeAsync(() => {
-  let counter = 0;
-
-  Promise.resolve().then(() => {
-    counter += 10;
-
-    setTimeout(() => {
-      counter += 1;
-    }, 1000);
-  });
-
-  expect(counter).toBe(0);
-
-  flushMicrotasks();
-
-  expect(counter).toBe(10);
-
-  tick(500);
-
-  expect(counter).toBe(10);
-
-  tick(500);
-
-  expect(counter).toBe(11);
-}));
